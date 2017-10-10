fb2e858f5a375cec10e324f9e997a89e40e9c8ee
Lee JongJin <leejongjin@Leeui-MacBook-Pro.local>
2017-10-11 03:52:59 +0900

    메모앱 화면 디자인 -구성, 제약조건 완성

---
+++ MyMemory/MemoFormVC.swift
@@ -0,0 +1,35 @@
+//
+//  MemoFormVC.swift
+//  MyMemory
+//
+//  Created by Lee JongJin on 2017. 10. 11..
+//  Copyright © 2017년 monk773. All rights reserved.
+//
+
+import UIKit
+
+class MemoFormVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
